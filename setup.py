3061d56f80f6bebc4a71661e0e20e45d59fde549
Joan Creus-Costa <jcreus@stanford.edu>
2019-05-20 13:25:27 -0700

    oh right I had a fix for this

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup, Extension
+import numpy as np
 
 setup(name='_xandri', version='0.01',  \
       ext_modules=[Extension('_xandri', ['py_interface/_xandri.c'],
-                   extra_objects=['build/libxandri.a'])])
+                   extra_objects=['build/libxandri.a'])],
+      include_dirs = [np.get_include()])
